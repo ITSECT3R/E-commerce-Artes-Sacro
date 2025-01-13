8c7f232fd5d81da96d30981871792a2756e5a5ea
ITSECT3R <ITSECT3R@GMAIL.COM>
2025-01-12 23:48:36 -0600

    completed card slider and added styles.

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,13 @@
 new Swiper('.card-wrapper', {
 
     loop: true,
+    spaceBetween: 30,
   
     // Pagination bullets
     pagination: {
       el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
     },
   
     // Navigation arrows
@@ -12,4 +15,17 @@ new Swiper('.card-wrapper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    }
+
   });
